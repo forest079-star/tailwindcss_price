385bf3f19e6218c945ae04be4b838a02f5e6ab90
forest079 <bluegx08@gmail.com>
2021-12-28 15:45:02 +0800

    更新 js html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: ['./src/**/*.html', './src/**/*.js'],
   mode: 'jit',
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
